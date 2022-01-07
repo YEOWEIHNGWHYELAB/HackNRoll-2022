f3a3aeb1c7e2427e580e70e978fa6679d1429d5a
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2022-01-07 22:25:44 +0800

    server initial config

--- server/server_const.py
+++ server/server_const.py
@@ -1,5 +1,10 @@
-import socket
-
 # Network Config
 HOST_IP_ADDRESS = socket.gethostbyname(socket.gethostname())
+HOST_GAME_PORT = 5556
 HOST_UPDATE_PORT = 5557
+
+# Shooting Multiplayer Game
+START_POS_P1_X = 0
+START_POS_P1_Y = 0
+START_POS_P2_X = 500
+START_POS_P2_Y = 400
